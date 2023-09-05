55c37d3a5fff8dc52da396d17b90afa1773f1667
jasper_a <andrea.nicole.jasper@gmail.com>
2023-09-05 13:00:03 -0600

    extends react component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Business from './components/Business';
 import BusinessList from './components/BusinessList';
 import SearchBar from './components/SearchBar';
 
-function App() {
-  return (
-    <div className="App">
-      <header>
-        <div className="container-fluid text-center">
-          <h1 className="logo">ravenous</h1>
-        </div>
-      </header>
-      <main>
-        <SearchBar />
-        <BusinessList />
-        {/* <Business /> */}
-      </main>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <div className="container-fluid text-center">
+            <h1 className="logo">ravenous</h1>
+          </div>
+        </header>
+        <main>
+          <SearchBar />
+          <BusinessList />
+          {/* <Business /> */}
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
